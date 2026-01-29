a8a109f73e1c9b7ad8048bc7a26cde8dcf5d759c
Komeil Parseh <komeilparseh@disroot.org>
2026-01-29 22:50:12 +0330

    update niri

--- .config/niri/niri_startup.sh
+++ .config/niri/niri_startup.sh
@@ -6,12 +6,17 @@
 # notifications
 dunst &
 
-# audio
+# Pipewire things
 pipewire &
 pipewire-pulse &
+
+/usr/libexec/xdg-desktop-portal-gtk &
 
 # clipboard
 wl-paste -t text --watch clipman store &
 
 # night light
 wlsunset -l 38.0792 -L 46.2887 -t 6000 &
+
+# audio stream
+setsid sh -c 'cd "$HOME/.local/bin" && ./pashare start 2>&1 | logger -t pashare' &
